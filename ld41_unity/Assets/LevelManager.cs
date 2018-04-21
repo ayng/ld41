6aacc87daba6c711b78625e0350da36c882cd2ae
Alex Yang <ayyang9@gmail.com>
2018-04-21 14:42:59 -0700

    start unity project

---
+++ ld41_unity/Assets/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3[] map0 = {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 2, 1)
+        };
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
