948cf0cc37ddfd901308f4d6a2acc4cb0a544e60
Alex Yang <ayyang9@gmail.com>
2018-04-21 21:12:21 -0700

    3d movement looks ok

--- ld41_unity/Assets/LevelManager.cs
+++ ld41_unity/Assets/LevelManager.cs
@@ -58,24 +58,24 @@ public class LevelManager : MonoBehaviour {
 
             Vector3 posFront = g_player.transform.position + (g_player.transform.rotation * Vector3.forward);
             Vector3 posAbove = posFront + Vector3.up;
-            Vector3 posBelow = posFront + Vector3.down;
 
             GameObject objFront = get(g_objects, posFront);
             GameObject objAbove = get(g_objects, posAbove);
-            GameObject objBelow = get(g_objects, posBelow);
 
-            Debug.LogFormat("front: {0} {1}", posFront, objFront != null);
-            Debug.LogFormat("above: {0} {1}", posAbove, objAbove != null);
-            Debug.LogFormat("below: {0} {1}", posBelow, objBelow != null);
+            // Debug.LogFormat("front: {0} {1}", posFront, objFront != null);
+            // Debug.LogFormat("above: {0} {1}", posAbove, objAbove != null);
 
-            if (objFront == null && objBelow != null) {
-                Debug.Log("forward");
-                g_player.transform.position = posFront;
-            } else if (objFront != null && objAbove == null) {
-                Debug.Log("climb");
-                g_player.transform.position = posAbove;
+            if (objFront != null) {
+                Debug.Log("detected object in front");
+                if (objAbove == null) {
+                    g_player.transform.position = posAbove;
+                }
             } else {
-                Debug.Log("can't move forward");
+                Debug.Log("nothing in front");
+                var objBelow = project(g_objects, posFront, Vector3.down);
+                if (objBelow != null) {
+                    g_player.transform.position = objBelow.transform.position + Vector3.up;
+                }
             }
         }
         if (Input.GetButtonDown("Down")) {
@@ -99,30 +99,44 @@ public class LevelManager : MonoBehaviour {
     }
 
     bool inBounds(Vector3 pos, GameObject[,,] objects) {
-        return 0 <= pos.y && pos.y < objects.GetLength(0)
-            && 0 <= pos.z && pos.z < objects.GetLength(1)
-            && 0 <= pos.x && pos.x < objects.GetLength(2)
+        return 0 <= round(pos.y) && round(pos.y) < objects.GetLength(0)
+            && 0 <= round(pos.z) && round(pos.z) < objects.GetLength(1)
+            && 0 <= round(pos.x) && round(pos.x) < objects.GetLength(2)
             ;
+    }
+
+    int round(float a) {
+        return (int) Mathf.Round(a);
     }
 
     GameObject get(GameObject[,,] objects, Vector3 pos) {
         if (!inBounds(pos, objects)) {
             return null;
         }
-        int x = (int) Mathf.Round(pos.x);
-        int y = (int) Mathf.Round(pos.y);
-        int z = (int) Mathf.Round(pos.z);
-        return objects[y, z, x];
+        return objects[round(pos.y), round(pos.z), round(pos.x)];
     }
 
     GameObject project(GameObject[,,] objects, Vector3 pos, Vector3 dir) {
-        for (var curPos = pos; inBounds(curPos, objects); curPos += dir) {
-            var curObj = get(objects, curPos);
+
+        int maxSteps = objects.GetLength(0) + objects.GetLength(1) + objects.GetLength(2);
+
+        int count = 0;
+        var curPos = pos;
+
+        GameObject curObj = null;
+        while (count < maxSteps) {
+            curObj = get(objects, curPos);
+
             if (curObj != null) {
-                return curObj;
+                break;
             }
+
+            count++;
+            curPos += dir;
         }
-        return null;
+
+        Debug.LogFormat("projection stepped {0} times", count);
+        return curObj;
     }
 
     // my dumb testing framework
@@ -142,6 +156,9 @@ public class LevelManager : MonoBehaviour {
         assert(get(objects, new Vector3(0, 1, 0)) == null);
         assert(get(objects, new Vector3(0, 1, 1)) != null);
         assert(get(objects, new Vector3(1, 1, 0)) == null);
+        assert(get(objects, new Vector3(1, 0, 0)) != null);
+        assert(get(objects, new Vector3(1, 0, 1)) != null);
+        assert(get(objects, new Vector3(0, 0, 1)) != null);
 
         // TODO
 
